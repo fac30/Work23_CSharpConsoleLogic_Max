6036a229ef7e3b265bc64e690cb695990f9184b5
maxitect <maxime.downe@gmail.com>
2024-11-06 16:46:47 +0000

    feat: module 3 if-else conversion to switch statement

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
-﻿int employeeLevel = 200;
-string employeeName = "John Smith";
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+using System.Security.Cryptography;
 
-string title = "";
+string sku = "01-MN-L";
 
-title = employeeLevel switch
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+type = product[0] switch
 {
-    100 => "Junior Associate",
-    200 => "Senior Associate",
-    300 => "Manager",
-    400 => "Senior Manager",
-    _ => "Associate",
+    "01" => "Sweat shirt",
+    "02" => "T-Shirt",
+    "03" => "Sweat pants",
+    _ => "Other"
 };
 
-Console.WriteLine($"{employeeName}, {title}");
+color = product[1] switch
+{
+    "BL" => "Black",
+    "MN" => "Maroon",
+    _ => "White"
+};
+
+size = product[2] switch
+{
+    "S" => "Small",
+    "M" => "Medium",
+    "L" => "Large",
+    _ => "One Size Fits All"
+};
+
+Console.WriteLine($"Product: {size} {color} {type}");
