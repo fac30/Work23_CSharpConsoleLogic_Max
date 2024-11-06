ff0b49f7b161806b04fa48263cdc26af1ba837aa
maxitect <maxime.downe@gmail.com>
2024-11-06 18:03:48 +0000

    feat: monster and hero attack game challenge module 4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
-﻿for (int num = 1; num <= 100; num++)
+﻿string[] characters = ["Monster", "Hero"];
+int[] charactersHealth = [10, 10];
+Random attack = new();
+int turnCount = 0;
+int damagedIndex;
+
+do
 {
-    Console.Write(num);
-    bool divisibleByThree = (num % 3 == 0);
-    bool divisibleByFive = (num % 5 == 0);
-    if (divisibleByThree || divisibleByFive)
-        Console.Write(" - ");
-    if (divisibleByThree)
-        Console.Write("Fizz");
-    if (divisibleByFive)
-        Console.Write("Buzz");
-    Console.Write($"\n");
-}
+    int damage = attack.Next(1, 11);
+    damagedIndex = turnCount % 2;
+    charactersHealth[damagedIndex] -= damage;
+    Console.WriteLine($"{characters[damagedIndex]} was damaged and lost {damage} health and now has {charactersHealth[damagedIndex]} health.");
+    turnCount++;
+} while (charactersHealth[0] > 0 && charactersHealth[1] > 0);
+
+Console.WriteLine($"{characters[Math.Abs(damagedIndex-1)]} wins!");
